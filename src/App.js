2eb084dc0e7755d4c3b8a0ab90690706005b10b7
Meraj <merajulhasan150@gmail.com>
2022-10-27 21:44:02 +0600

    App.js upload

--- src/App.js
+++ src/App.js
@@ -1,2 +1,18 @@
+import './App.css';
+import { RouterProvider } from 'react-router-dom';
+import { routes } from './Routers/Routes';
+//import { router } from './Routers/Router/Router';
+
+function App() {
+
+  return (
+    <div className="App">
+      <RouterProvider router={routes}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
+
 
 
